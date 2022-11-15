d863186509f42fc170d4aec9251464431ec08ef1
bofmar <bofmar@outlook.com>
2022-11-15 14:49:57 +0200

    Setup the needed components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world</h1>
+      <Navbar />
+      <Main />
     </div>
   );
 }
