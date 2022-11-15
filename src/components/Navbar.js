583be1b40785967d0d0d6bdd9ce99afb53e2dfe4
bofmar <bofmar@outlook.com>
2022-11-15 15:33:08 +0200

    Finish the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,15 @@
 import React from "react";
+import logo from '../logo.svg';
 
 export default function Navbar() {
+  console.log(logo);
   return (
-    <h1>Navbar</h1>
+    <nav>
+      <section className='logo-section'>
+        <img src={logo} className='logo' />
+        <h3>ReactFacts</h3>
+      </section>
+      <h4>React Course - Project 1</h4>
+    </nav>
   );
 }
