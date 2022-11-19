db5ac98ae6e6e89697db67d3082e1d03eaf943d4
bofmar <bofmar@outlook.com>
2022-11-19 12:36:37 +0200

    Implement dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = React.useState(false);
+
+  function toggle() {
+    setIsDarkMode(prevMode => !prevMode);
+  }
+
   return (
     <div className="App">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={isDarkMode} toggleDarkMode={toggle} />
+      <Main darkMode={isDarkMode} />
     </div>
   );
 }
